95d2cb8f13eb2231004f616b5456ba5cb4b8093c
Ali Beheshti <149072121+mr-alibeheshti@users.noreply.github.com>
2024-07-04 09:18:55 +0330

    Add files via upload

--- src/utils/csvExport.ts
+++ src/utils/csvExport.ts
@@ -1,15 +1,13 @@
 import { createObjectCsvWriter } from 'csv-writer';
 import Website from '../models/websites';
-import { isUtf8 } from 'buffer';
-
 export const exportCSV = async (fields: string[]): Promise<string> => {
   const websites = await Website.find().select(fields.join(' '));
+  console.log(websites);
   const csvWriter = createObjectCsvWriter({
     path: 'data.csv',
-    encoding :'utf-8',
     header: fields.map(field => ({ id: field, title: field }))
   });
 
   await csvWriter.writeRecords(websites.map(site => site.toObject()));
-  return 'websites.csv';
+  return 'data.csv'
 };
